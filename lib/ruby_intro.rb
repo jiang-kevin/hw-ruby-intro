e24b4acb4c9f1f92752fd20b3f31c0325f0ab484
Kevin Jiang <kjiang1998@gmail.com>
2020-01-03 08:37:30 +0000

    Finished assignment

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,82 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(array)
+  output = 0
+  array.each do |num|
+    output += num
+  end
+  output
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(array)
+  max_2 = array.max(2)
+  sum(max_2)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.each_with_index do |a, i|
+    arr.each_with_index do |b, j|
+      if (i != j)
+        if (a + b == n)
+          return true
+        end
+      end
+    end
+  end
+  
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[qwrtypsdfghjklzxcvbnm]/i =~ s
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if (/^[01]+$/ =~ s)
+    s.to_i(2) % 4 == 0
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    self.isbn = isbn
+    self.price = price
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def isbn=(isbn)
+    if (isbn == "")
+      raise ArgumentError.new()
+    else
+      @isbn = isbn
+    end
+  end
+  
+  def price
+    @price
+  end
+  
+  def price=(price)
+    if (price <= 0)
+      raise ArgumentError.new()
+    else
+      @price = price
+    end
+  end
+  
+  def price_as_string()
+    sprintf('$%.2f', @price)
+  end
 end
